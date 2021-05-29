a2770cc52e7d1f52a35d136009108a662cfd67c4
Alex <stankovic.aleksandra@protonmail.ch>
2021-05-29 23:28:53 +0200

    29.5.21 kraj dana

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 200) {
+    if ($(document).scrollTop() > 50) {
       $('.nav_bg').addClass('dark');
     } else {
         $('.nav_bg').removeClass('transparent');
       }
-    if($(document).scrollTop() < 200){
+    if($(document).scrollTop() < 50){
         $('.nav_bg').removeClass('dark');
     } 
 
