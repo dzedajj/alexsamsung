2663212791fab8fa892eb838e5379de9f2b1dbc2
Alex <stankovic.aleksandra@protonmail.ch>
2021-06-02 00:36:27 +0200

    2.6.21. kraj dana

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
+    if ($(document).scrollTop() > 100) {
       $('.nav_bg').addClass('dark');
     } else {
         $('.nav_bg').removeClass('transparent');
       }
-    if($(document).scrollTop() < 50){
+    if($(document).scrollTop() < 100){
         $('.nav_bg').removeClass('dark');
     } 
 
     if ($(window).width() < 500) {
-        if ($(document).scrollTop() > 50) {
+        if ($(document).scrollTop() > 100) {
             $('.nav_bg').addClass('dark');
           } else {
               $('.nav_bg').removeClass('transparent');
             }
-          if($(document).scrollTop() < 50){
+          if($(document).scrollTop() < 100){
               $('.nav_bg').removeClass('dark');
           } 
     }
