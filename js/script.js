abd4e12aec3224ded51ad26a6221bd4cf43a176e
Dzedajj <petkovic.jovan55@gmail.com>
2021-06-07 18:43:18 +0200

    jovanova scripta

--- js/script.js
+++ js/script.js
@@ -1,35 +1,37 @@
-$(window).scroll(function() {
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 100) {
+    $('.nav_bg').addClass('dark');
+  } else {
+    $('.nav_bg').removeClass('transparent');
+  }
+  if ($(document).scrollTop() < 100) {
+    $('.nav_bg').removeClass('dark');
+  }
+
+  if ($(window).width() < 500) {
     if ($(document).scrollTop() > 100) {
       $('.nav_bg').addClass('dark');
     } else {
-        $('.nav_bg').removeClass('transparent');
-      }
-    if($(document).scrollTop() < 100){
-        $('.nav_bg').removeClass('dark');
-    } 
-
-    if ($(window).width() < 500) {
-        if ($(document).scrollTop() > 100) {
-            $('.nav_bg').addClass('dark');
-          } else {
-              $('.nav_bg').removeClass('transparent');
-            }
-          if($(document).scrollTop() < 100){
-              $('.nav_bg').removeClass('dark');
-          } 
+      $('.nav_bg').removeClass('transparent');
     }
+    if ($(document).scrollTop() < 100) {
+      $('.nav_bg').removeClass('dark');
+    }
+  }
+
+  if ($(document).scrollTop() < 400) {
+    $('.toTheTop').css('display', 'none');
+  } else {
+    $('.toTheTop').css('display', 'block');
+  }
+
+  if ($(window).width() < 990 && $(document).scrollTop() === 0) {
+    const dugme = document.querySelector;
+
+    const poz = document.querySelector('.nav_bg');
+
+    $('.navbar-toggler').addEventListener('click', () => {
+      $('.nav_bg').css('background', '#1e0353');
+    });
+  }
 });
-
-
-
-
-if ($(window).width() < 990) {
- const dugme = document.querySelector('.navbar-toggler');
-  
-  const poz = document.querySelector('.nav_bg');
-    
-  dugme.addEventListener('click' , () => {
-    poz.style.background = "#1e0353";
-  })
-}
-
