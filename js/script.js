8cef3da0aedd03e99a15eca5547437332f536934
Alex <stankovic.aleksandra@protonmail.ch>
2021-06-08 16:02:50 +0200

    gotovo skoro

--- js/script.js
+++ js/script.js
@@ -1,39 +1,24 @@
+// Dok se scrolluje kroz window (DOM element)
 $(window).scroll(function () {
+
+  // Navbar background boja da se pojavljuje
   if ($(document).scrollTop() > 100) {
     $('.nav_bg').addClass('dark');
   } else {
-    $('.nav_bg').removeClass('transparent');
-  }
-  if ($(document).scrollTop() < 100) {
     $('.nav_bg').removeClass('dark');
   }
 
-  if ($(window).width() < 500) {
-    if ($(document).scrollTop() > 100) {
-      $('.nav_bg').addClass('dark');
-    } else {
-      $('.nav_bg').removeClass('transparent');
-    }
-    if ($(document).scrollTop() < 100) {
-      $('.nav_bg').removeClass('dark');
-    }
-  }
-
+  // Dugme to the top da se pojavi
   if ($(document).scrollTop() < 400) {
     $('.toTheTop').css('display', 'none');
   } else {
     $('.toTheTop').css('display', 'block');
   }
-
-  if ($(window).width() < 990 && $(document).scrollTop() === 0) {
-    const dugme = document.querySelector;
-
-    const poz = document.querySelector('.nav_bg');
-
-    $('.navbar-toggler').addEventListener('click', () => {
-      $('.nav_bg').css('background', '#1e0353');
-    });
-  }
 });
 
-
+// Da kad se klikne hamburger dobije pozadinu
+if ($(document).scrollTop() == 0) {
+  $('.navbar-toggler').click( function() {
+    $('.nav_bg').css('background', '#1e0353');
+  });
+}
